8523d50c2517465b3e3c7804957df9d33b9ae073
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-01-12 23:27:55 -0800

    Add ProfileScreen and logout functionality

--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,5 +1,6 @@
 import './Dashboard.css'
 import ProjectSection from '../features/projects/components/ProjectSection.jsx';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
 
@@ -7,6 +8,9 @@ const Dashboard = () => {
     <div className="window">
       <div className="sidebar">
         <ProjectSection />
+        <div className='profile-section'>
+          <Link to='/profile'>Profile</Link>
+        </div>
       </div>  
       <div className="content"></div>
     </div>
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,27 +1,37 @@
+import './ProfileScreen.css'
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import { setCredentials } from "../slices/authSlice"
-import { useUpdateUserMutation } from '../slices/usersApiSlice'
+import { useUpdateUserMutation, useLogoutMutation } from '../slices/usersApiSlice'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout } from '../slices/authSlice'
 
 const ProfileScreen = () => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	// State
   const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
+	const [email, setEmail] = useState('') 
 	const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-
 	const { userInfo } = useSelector((state) => state.auth)
 
+	// Query & Mutations
 	const [updateProfile, { isLoading }] = useUpdateUserMutation()
+	const [logoutApiCall] = useLogoutMutation()
 
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		if (userInfo) {
-			setName(userInfo.name)
-			setEmail(userInfo.email)
+	// API Call Functions
+	const logoutHandler = async () => {
+		try {
+			await logoutApiCall().unwrap()
+			dispatch(logout())
+			navigate('/')
+		} catch (error) {
+			console.log(error)
 		}
-	}, [userInfo])
+	}
 
 	const submitHandler = async (e) => {
 		e.preventDefault()
@@ -44,60 +54,82 @@ const ProfileScreen = () => {
 	}
 
 
+	// Use Effect
+	useEffect(() => {
+		if (userInfo) {
+			setName(userInfo.name)
+			setEmail(userInfo.email)
+		}
+	}, [userInfo])
+
+
+
   return (
-    <div className="form-container">
-			<h1>Update Profile</h1>
-			<form onSubmit={ submitHandler }>
-                
-        <div className="form-row">
-					<label>Name</label>
-					<input  
-						type="text" 
-						placeholder="Enter First Name..." 
-						value={name} 
-						onChange={(e) => setName(e.target.value)}
-					>
-					</input>
-				</div>
-				
-				<div className="form-row">
-					<label>Email Address</label>
-					<input 
-						type="email" 
-						placeholder="Enter Email..." 
-						value={email} 
-						onChange={(e) => setEmail(e.target.value)}
-					>
-					</input>
-				</div>
+    <div className="container">
+			<div className="form-container">
+				<h1 className='form-title'>Update Profile</h1>
+				<form onSubmit={ submitHandler }>
+	
+					<div className="form-row">
+						<label>Name</label>
+						<input  
+							type="text" 
+							placeholder="Enter First Name..." 
+							value={name} 
+							onChange={(e) => setName(e.target.value)}
+						>
+						</input>
+					</div>
+					
+					<div className="form-row">
+						<label>Email Address</label>
+						<input 
+							type="email" 
+							placeholder="Enter Email..." 
+							value={email} 
+							onChange={(e) => setEmail(e.target.value)}
+						>
+						</input>
+					</div>
+	
+					<div className="form-row">
+						<label>Password</label>
+						<input 
+							type="password" 
+							placeholder="Enter Password..." 
+							value={password} 
+							onChange={(e) => setPassword(e.target.value)}
+						>
+						</input>
+					</div>
+	
+					<div className="form-row">
+						<label>Confirm Password</label>
+						<input 
+							type="password" 
+							placeholder="Confirm Password..." 
+							value={confirmPassword} 
+							onChange={(e) => setConfirmPassword(e.target.value)}
+						>
+						</input>
+					</div>
+	
+					{ isLoading && <p>Loading...</p>}
+	
+					<button type="submit">Update</button>
 
-				<div className="form-row">
-					<label>Password</label>
-					<input 
-						type="password" 
-						placeholder="Enter Password..." 
-						value={password} 
-						onChange={(e) => setPassword(e.target.value)}
-					>
-					</input>
-				</div>
+					<button 
+						type='button' 
+						className='logout-btn'
+						onClick={() => logoutHandler()}
+						>
+						Logout
+					</button>
 
-				<div className="form-row">
-					<label>Confirm Password</label>
-					<input 
-						type="password" 
-						placeholder="Confirm Password..." 
-						value={confirmPassword} 
-						onChange={(e) => setConfirmPassword(e.target.value)}
-					>
-					</input>
-				</div>
-
-				{ isLoading && <p>Loading...</p>}
-
-				<button type="submit">Update</button>
-
-			</form>
+					<Link to='/'><button type='button' className='back-btn'>Back</button></Link>
+	
+				</form>
+			</div>
 		</div>
   )
 }
